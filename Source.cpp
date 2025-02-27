42b14e72870cf387d26c96a1a8040d6ca5652de7
tikhomirovofficial <arteom.tixomirow@yandex.ru>
2025-02-27 14:18:23 +0300

    dialog fix

--- Source.cpp
+++ Source.cpp
@@ -24,15 +24,23 @@ struct AEROFLOT {
 
 
 /* FUNCTIONS DECLARATIONS */
+
+
+
 void printDeportureDateTimes(Date date, Time time) {
 	cout << "date: " << date.day << "." << date.month << "." << date.year << endl;
 	cout << "time: " << time.hours << ":" << time.minutes << endl << endl;
 }
 
-void printLine(int length = 10) {
-	cout << endl;
-	for (int i = 0; i < length; i++) cout << i;
-	cout << endl;
+void printLineOfChars(int length = 50, char symbol = '-') {
+	for (int i = 0; i < length; i++) cout << symbol;
+	cout << endl << endl;
+}
+
+void printGreeting() {
+	cout << "**** WELCOME DEPORTURES MANAGER BY TIKHOMIROV ****" << endl << endl;
+	cout << "YOU SHOULD TO FILL DATA FOR " << DEPORTURES_LEN << " DEPORTURES" << endl;
+	printLineOfChars();
 }
 
 void printDeporturesByName(AEROFLOT deportures[], string dest_name) {
@@ -50,18 +58,19 @@ void printDeporturesByName(AEROFLOT deportures[], string dest_name) {
 		}
 	}
 
-	if (!findedMoreZero) cout << "BY QUERY '" << dest_name << "' NOTHING WAS FOUND.";
+	if (!findedMoreZero) cout << "BY QUERY '" << dest_name << "' NOTHING WAS FOUND." << endl;
 }
 
 AEROFLOT enterDeporture() {
 	AEROFLOT deportureResult;
 
 	cout << "Enter name: "; cin >> deportureResult.destination;
-	cout << "Enter day"; cin >> deportureResult.dateDeporture.day;
-	cout << "Enter month (number) "; cin >> deportureResult.dateDeporture.month;
-	cout << "Enter (year) "; cin >> deportureResult.dateDeporture.year;
-	cout << "Enter hours "; cin >> deportureResult.timeDeporture.hours;
-	cout << "Enter minutes (number) "; cin >> deportureResult.timeDeporture.minutes; cout << endl;
+	cout << "Enter day: "; cin >> deportureResult.dateDeporture.day;
+	cout << "Enter month (number): "; cin >> deportureResult.dateDeporture.month;
+	cout << "Enter year: "; cin >> deportureResult.dateDeporture.year;
+	cout << "Enter hours: "; cin >> deportureResult.timeDeporture.hours;
+	cout << "Enter minutes: "; cin >> deportureResult.timeDeporture.minutes; cout << endl;
+	printLineOfChars();
 
 	return deportureResult;
 }
@@ -69,22 +78,20 @@ AEROFLOT enterDeporture() {
 
 
 int main() {
-
-	cout << "**** WELCOME DEPORTURES MANAGER BY TIKHOMIROV ****" << endl << endl;
-	cout << "YOU SHOULD TO FILL DATA FOR " << DEPORTURES_LEN << " DEPORTURES" << endl << endl;
+	printGreeting();
 	AEROFLOT deportures[DEPORTURES_LEN];
 
 	for (int i = 0; i < DEPORTURES_LEN; i++)
 	{
-		cout << "ENTER DATA FOR" << i + 1 << " DEPORTURE:" << endl << endl;
-		
+		cout << "ENTER DATA FOR " << i + 1 << " DEPORTURE:" << endl << endl;
 		deportures[i] = enterDeporture();
 	}
 
 	string queryName;
-	cout << "SEARCHING MODE. PLEASE ENTER DESTINATION NAME: "; cin >> queryName;
+	cout << "*** SEARCHING MODE ***" << endl; "PLEASE ENTER DESTINATION NAME: "; cin >> queryName;
 
 	printDeporturesByName(deportures, queryName);
+	printLineOfChars();
 
 	return 0;
 }
